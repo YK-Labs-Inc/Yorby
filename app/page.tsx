017dd5ff25bbbe576374e6ced558189201faae2c
Dohyun Kim <dohyun@youraveragetechbro.com>
2024-12-31 12:22:44 -0600

    fix search params type error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 export default async function Home({
   searchParams,
 }: {
-  searchParams: { signed_up?: string };
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const signUpToWaitlist = async (formData: FormData) => {
     "use server";
@@ -18,8 +18,8 @@ export default async function Home({
     }
     redirect("/?signed_up=true");
   };
-
-  if (searchParams.signed_up === "true") {
+  const signedUp = (await searchParams).signed_up === "true";
+  if (signedUp) {
     return (
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h1 className="text-4xl font-bold tracking-tight text-center">
