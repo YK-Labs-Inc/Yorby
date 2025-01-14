e76e471c785458ab4a147dd73c0f7133102fc93c
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-01-14 10:35:06 -0600

    Render previous user submissions

--- app/[locale]/dashboard/jobs/[jobId]/[questionId]/page.tsx
+++ app/[locale]/dashboard/jobs/[jobId]/[questionId]/page.tsx
@@ -42,7 +42,6 @@ export default async function Page({
         question={question}
         submissions={question.custom_job_question_submissions}
       />
-      <AnswerGuideline question={question} />
     </div>
   );
 }
