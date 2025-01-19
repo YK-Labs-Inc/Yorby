5538d2482f16c69f0fc6f179177594c61ea15e42
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-01-18 21:59:59 -0600

    Add support to link anonymous user to real user

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -5,19 +5,21 @@ export type Message =
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div className="flex flex-col gap-2 w-full max-w-md text-sm">
+    <div className="flex flex-col gap-2 w-full text-sm">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className="bg-green-100 text-green-800 rounded-md px-4 py-3">
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="bg-red-100 text-red-800 rounded-md px-4 py-3">
           {message.error}
         </div>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="bg-gray-100 text-gray-800 rounded-md px-4 py-3">
+          {message.message}
+        </div>
       )}
     </div>
   );
