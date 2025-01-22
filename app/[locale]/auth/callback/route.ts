dcf4ea04e62732d5cfcbb2899f452c5e07fa68e3
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-01-22 14:46:09 -0600

    Add linked user to brevo

--- app/[locale]/auth/callback/route.ts
+++ app/[locale]/auth/callback/route.ts
@@ -1,6 +1,8 @@
 import { createSupabaseServerClient } from "@/utils/supabase/server";
-import { AxiomRequest, withAxiom } from "next-axiom";
+import { AxiomRequest, Logger, withAxiom } from "next-axiom";
 import { NextResponse } from "next/server";
+import * as SibApiV3Sdk from "@sendinblue/client";
+import { trackServerEvent } from "@/utils/tracking/serverUtils";
 
 export const GET = withAxiom(async (request: AxiomRequest) => {
   // The `/auth/callback` route is required for the server-side auth flow implemented
@@ -12,7 +14,7 @@ export const GET = withAxiom(async (request: AxiomRequest) => {
   const email = requestUrl.searchParams.get("email");
   const origin = requestUrl.origin;
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
-  const logger = request.log.with({
+  let logger = request.log.with({
     type,
     email,
     origin,
@@ -29,6 +31,7 @@ export const GET = withAxiom(async (request: AxiomRequest) => {
         `${origin}/sign-in?error=Invalid or expired code. Please try again.`
       );
     }
+    logger.info("Handling magic link");
     // Handle magic link flow
     const { error } = await supabase.auth.verifyOtp({
       token_hash: token,
@@ -60,6 +63,17 @@ export const GET = withAxiom(async (request: AxiomRequest) => {
       token_hash: token,
       type: "email_change",
     });
+    const { data: userData } = await supabase.auth.getUser();
+    const user = userData?.user;
+    logger = logger.with({ userId: user?.id });
+    logger.info("Handling user email change");
+    if (user?.id && email) {
+      await addUserToBrevo({
+        userId: user.id,
+        email,
+        logger,
+      });
+    }
     if (error) {
       logger.error("Failed to verify email change", { error });
       return NextResponse.redirect(
@@ -76,3 +90,36 @@ export const GET = withAxiom(async (request: AxiomRequest) => {
   logger.info("Redirecting to jobs dashboard");
   return NextResponse.redirect(`${origin}/dashboard/jobs`);
 });
+
+const addUserToBrevo = async ({
+  userId,
+  email,
+  logger,
+}: {
+  userId: string;
+  email: string;
+  logger: Logger;
+}) => {
+  let apiInstance = new SibApiV3Sdk.ContactsApi();
+  const brevoApiKey = process.env.BREVO_API_KEY;
+  if (!brevoApiKey) {
+    throw new Error("BREVO_API_KEY is not set");
+  }
+  apiInstance.setApiKey(SibApiV3Sdk.ContactsApiApiKeys.apiKey, brevoApiKey);
+
+  let createContact = new SibApiV3Sdk.CreateContact();
+
+  createContact.email = email;
+  createContact.listIds = [6];
+
+  await apiInstance.createContact(createContact);
+  logger.info("Added contact to Brevo");
+  await trackServerEvent({
+    userId,
+    email,
+    eventName: "user_sign_up",
+    args: {
+      email,
+    },
+  });
+};
