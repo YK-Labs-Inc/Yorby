ee68f84c01151f20189ff5f3fff49edf9f49f589
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-01-22 13:05:39 -0600

    Remove waitlist

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,9 @@
 import LandingPageV2 from "./LandingPageV2";
-import WaitlistComponent from "./WaitlistComponent";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const isDev = (await searchParams).dev === "true";
+export default async function Home() {
   return (
     <div className="flex flex-col gap-6 max-w-[1080px] mx-auto justify-center min-h-screen items-center">
-      {isDev ? <LandingPageV2 /> : <WaitlistComponent />}
+      <LandingPageV2 />
     </div>
   );
 }
