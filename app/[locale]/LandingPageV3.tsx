fcd2e91e93d41b96dd0f7cd5e4d115017f76ea3e
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2025-02-23 15:58:38 -0600

    landing page update for more mobile support (#95)

--- app/[locale]/LandingPageV3.tsx
+++ app/[locale]/LandingPageV3.tsx
@@ -3,7 +3,7 @@ import FeatureHighlightsV2 from "./components/FeatureHighlightsV2";
 
 export default function LandingPageV3() {
   return (
-    <div className="flex flex-col justify-center min-h-screen w-full mt-8 p-4">
+    <div className="flex flex-col justify-center min-h-screen w-full mt-8 p-1">
       <FeatureHighlightsV2 />
       <FAQ />
     </div>
