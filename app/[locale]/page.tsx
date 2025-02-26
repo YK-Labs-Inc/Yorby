02fd89ebbeb199a56606db928d0021f9fce359af
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2025-02-26 07:18:42 -0600

    Move to v3 landing page (#99)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,9 @@
-import LandingPageV2 from "./LandingPageV2";
 import LandingPageV3 from "./LandingPageV3";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  const v3 = (await searchParams).v3 === "true";
-
+export default async function Home() {
   return (
     <div className="flex flex-col gap-6 max-w-[1080px] mx-auto justify-center min-h-screen items-center">
-      {v3 ? <LandingPageV3 /> : <LandingPageV2 />}
+      <LandingPageV3 />
     </div>
   );
 }
