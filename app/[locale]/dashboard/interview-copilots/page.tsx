cd3b5b03e413605074546c953385d4f5346008c4
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-03-10 17:18:01 -0500

    save wip

--- app/[locale]/dashboard/interview-copilots/page.tsx
+++ app/[locale]/dashboard/interview-copilots/page.tsx
@@ -25,7 +25,7 @@ export default async function InterviewCopilotsPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    redirect("/login");
+    redirect("/sign-in");
   }
 
   const userCredits = await fetchUserCredits(user.id);
