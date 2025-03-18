402410c0b00108898b39d0cb2571e1d9695e4f81
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-03-18 06:50:05 -0500

    Remove generate static paths

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,18 +5,6 @@ import ScrollProgressBar from "../../components/ScrollProgressBar";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-export async function generateStaticParams() {
-  const baseUrl =
-    process.env.NEXT_PUBLIC_BASE_URL || "https://perfectinterview.ai";
-  const posts = await fetch(`${baseUrl}/api/demo-jobs`).then((res) =>
-    res.json()
-  );
-
-  return posts.data.map((post: Tables<"demo_jobs">) => ({
-    slug: post.slug,
-  }));
-}
-
 type DemoJobWithQuestions = Tables<"demo_jobs"> & {
   demo_job_questions: Tables<"demo_job_questions">[];
 };
