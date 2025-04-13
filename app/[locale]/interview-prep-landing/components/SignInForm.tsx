04b8843a8c53f05ad0b09e45cfd5dd7c92fc343d
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-04-13 10:28:43 -0500

    fire event on landing page magic link enter

--- app/[locale]/interview-prep-landing/components/SignInForm.tsx
+++ app/[locale]/interview-prep-landing/components/SignInForm.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { FormMessage, Message } from "@/components/form-message";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { Input } from "@/components/ui/input";
 import { SubmitButton } from "@/components/submit-button";
-import { useActionState, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 import { Turnstile } from "@marsidev/react-turnstile";
 import { signInWithOTP } from "../../(auth-pages)/actions";
 import { H4 } from "@/components/typography";
+import { usePostHog } from "posthog-js/react";
 
 export default function SignInForm() {
   const signInT = useTranslations("signIn");
@@ -18,12 +19,27 @@ export default function SignInForm() {
   });
   const [captchaToken, setCaptchaToken] = useState<string>("");
   const [showCaptcha, setShowCaptcha] = useState<boolean>(true);
+  const posthog = usePostHog();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   let formMessage: Message | undefined;
   if (state.success) {
     formMessage = { success: state.success };
   } else if (state.error) {
     formMessage = { error: state.error };
   }
+
+  useEffect(() => {
+    if (state.success) {
+      let url = window.origin + pathname;
+      if (searchParams && searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture("landing_page_magic_link_entered", {
+        $current_url: url,
+      });
+    }
+  }, [state]);
 
   return (
     <div className="container max-w-md mx-auto">
