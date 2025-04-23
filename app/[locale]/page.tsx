36521e29c5716a56018d6d9361816649432b28d7
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2025-04-23 14:26:59 -0400

    Create new landing page v6 (#224)
    * Create new landing page v6
    
    * update

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,13 +5,19 @@ import {
   fetchHasSubscription,
 } from "./dashboard/resumes/actions";
 import LandingPageV5 from "./LandingPageV5";
+import LandingPageV6 from "./LandingPageV6";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
   const supabase = await createSupabaseServerClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const v6 = (await searchParams)?.["v6"] === "true";
   let hasSubscription = false;
   let credits = 0;
   let isSubscriptionVariant = false;
@@ -32,6 +38,19 @@ export default async function Home() {
       false;
     enableResumesFileUpload =
       (await posthog.isFeatureEnabled("enable-memories", user.id)) ?? false;
+  }
+  if (v6) {
+    return (
+      <LandingPageV6
+        user={user}
+        hasSubscription={hasSubscription}
+        credits={credits}
+        isSubscriptionVariant={isSubscriptionVariant}
+        isFreemiumEnabled={isFreemiumEnabled}
+        transformResumeEnabled={transformResumeEnabled}
+        enableResumesFileUpload={enableResumesFileUpload}
+      />
+    );
   }
   return (
     <LandingPageV5
