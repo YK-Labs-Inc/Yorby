d98ca92d06c2452a50a7758e32e74a4ca1d7d828
YourAverageTechBro <94861246+YourAverageTechBro@users.noreply.github.com>
2025-05-10 09:55:36 -0500

    fix supabase migrations (#260)
    * fix supabase migrations
    
    * attempt to fix migrations
    
    * fix migrations
    
    * fix
    
    * enable webhooks in config.toml
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- supabase/migrations/20250508135232_add-slug-to-coaches.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."coaches" add column "slug" text not null;
-
-CREATE UNIQUE INDEX coaches_slug_key ON public.coaches USING btree (slug);
-
-alter table "public"."coaches" add constraint "coaches_slug_key" UNIQUE using index "coaches_slug_key";
-
-
--- supabase/migrations/20250509142703_fix-supabase-edge-function.sql
+++
@@ -1,5 +0,0 @@
-drop trigger if exists "New job " on "public"."custom_jobs";
-
-CREATE TRIGGER "New job " AFTER INSERT ON public.custom_jobs FOR EACH ROW EXECUTE FUNCTION supabase_functions.http_request('https://www.perfectinterview.ai/api/webhooks/supabase/new-custom-job', 'POST', '{"Content-type":"application/json","authorization":"Bearer AnCJ789rcEzFVF2npAogMVyq6OXdnCcY"}', '{}', '10000');
-
-
