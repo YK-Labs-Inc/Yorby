2ba87bebcd7490c3ba037b0f04b1cefcf1608752
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-05-12 18:40:09 -0500

    Fire posthog complete registration event with meta

--- app/[locale]/confirm-initial-login/page.tsx
+++ app/[locale]/confirm-initial-login/page.tsx
@@ -2,19 +2,24 @@
 
 import { useUser } from "@/context/UserContext";
 import { useRouter } from "next/navigation";
+import { usePostHog } from "posthog-js/react";
 import { useEffect, useRef } from "react";
 
 const ConfirmInitialLogin = () => {
   const user = useUser();
   const hasSentRegistrationEvent = useRef(false);
   const router = useRouter();
+  const posthog = usePostHog();
   useEffect(() => {
-    if (user && !hasSentRegistrationEvent.current) {
+    if (posthog && user && !hasSentRegistrationEvent.current) {
       hasSentRegistrationEvent.current = true;
       window.fbq("track", "CompleteRegistration");
+      posthog.capture("CompleteRegistration_ph", {
+        userId: user.id,
+      });
       router.push("/onboarding-v2");
     }
-  }, [user]);
+  }, [posthog, user]);
   return null;
 };
 
