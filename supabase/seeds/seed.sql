399ec2d0d69c6a31c8f2f1ce02de8bc3c05985c7
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-05-19 15:52:53 -1000

    updat seed script

--- supabase/seeds/seed.sql
+++ supabase/seeds/seed.sql
@@ -454,6 +454,17 @@ INSERT INTO "public"."custom_job_question_submissions" ("id", "created_at", "cus
 	('99999999-9999-9999-9999-999999999999', NOW(), '55a20866-8315-4aa6-9020-10080a1d5014', 'lorem ipsum', '{"pros": ["lorem ipsum", "lorem ipsum"], "cons": ["lorem ipsum", "lorem ipsum"]}');
 
 
+INSERT INTO "public"."custom_job_question_submission_feedback" ("id", "created_at", "submission_id", "feedback_role", "pros", "cons") VALUES
+	('11111111-1111-1111-1111-111111111111', NOW(), '11111111-1111-1111-1111-111111111111', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('22222222-2222-2222-2222-222222222222', NOW(), '22222222-2222-2222-2222-222222222222', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('33333333-3333-3333-3333-333333333333', NOW(), '33333333-3333-3333-3333-333333333333', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('44444444-4444-4444-4444-444444444444', NOW(), '44444444-4444-4444-4444-444444444444', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('55555555-5555-5555-5555-555555555555', NOW(), '55555555-5555-5555-5555-555555555555', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('66666666-6666-6666-6666-666666666666', NOW(), '66666666-6666-6666-6666-666666666666', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('77777777-7777-7777-7777-777777777777', NOW(), '77777777-7777-7777-7777-777777777777', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('88888888-8888-8888-8888-888888888888', NOW(), '88888888-8888-8888-8888-888888888888', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']),
+	('99999999-9999-9999-9999-999999999999', NOW(), '99999999-9999-9999-9999-999999999999', 'ai', ARRAY['lorem ipsum new table', 'lorem ipsum'], ARRAY['lorem ipsum', 'lorem ipsum']);
+
 --
 -- Data for Name: demo_jobs; Type: TABLE DATA; Schema: public; Owner: postgres
 --
