08879e9bb64bc5ac1a8fdffaa79adf15466c4cc5
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-05-28 20:59:35 -0500

    fix

--- supabase/migrations/20250529015431_remote_schema.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."custom_job_questions" alter column "publication_status" set default 'published'::question_publication_status;
-
-
--- supabase/migrations/20250529015628_add-coach-read-access-for-student-audio-files.sql
+++
@@ -1,9 +0,0 @@
-create policy "Give users access to coaches folder of a user 84ce0l_0"
-on "storage"."objects"
-as permissive
-for select
-to public
-using (((bucket_id = 'user-files'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[3])));
-
-
-
