9e79c6af072808b991c252de90af70b4d7635d1a
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-06-04 21:57:22 -0500

    fix

--- app/purchase_confirmation/page.tsx
+++ app/purchase_confirmation/page.tsx
@@ -3,7 +3,7 @@ import { Logger } from "next-axiom";
 import RedirectMessage from "./RedirectMessage";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export default async function PurchaseConfirmation({
