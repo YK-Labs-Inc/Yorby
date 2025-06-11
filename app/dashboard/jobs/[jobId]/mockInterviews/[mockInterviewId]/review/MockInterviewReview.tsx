2aa9d70c6269ccab4d2f16544dd18af67313c81a
Dohyun Kim <dohyun@youraveragetechbro.com>
2025-06-10 20:40:48 -0500

    Play mux video if it is available

--- app/dashboard/jobs/[jobId]/mockInterviews/[mockInterviewId]/review/MockInterviewReview.tsx
+++ app/dashboard/jobs/[jobId]/mockInterviews/[mockInterviewId]/review/MockInterviewReview.tsx
@@ -1,9 +1,14 @@
 import MockInterviewReviewClientComponent from "./MockInterviewReviewClientComponent";
 import { trackServerEvent } from "@/utils/tracking/serverUtils";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
+import { Logger } from "next-axiom";
 
 async function fetchMockInterviewData(mockInterviewId: string) {
   const supabase = await createSupabaseServerClient();
+  const logger = new Logger().with({
+    function: "fetchMockInterviewData",
+    mockInterviewId,
+  });
 
   // Fetch mock interview data
   const { data: mockInterview, error: mockInterviewError } = await supabase
@@ -12,16 +17,28 @@ async function fetchMockInterviewData(mockInterviewId: string) {
     .eq("id", mockInterviewId)
     .single();
 
-  if (mockInterviewError) throw mockInterviewError;
+  if (mockInterviewError) {
+    logger.error("Error fetching mock interview data", {
+      mockInterviewError,
+    });
+    await logger.flush();
+    throw mockInterviewError;
+  }
 
   // Fetch messages
   const { data: messages, error: messagesError } = await supabase
     .from("mock_interview_messages")
-    .select("*")
+    .select("*, mux_metadata:mock_interview_message_mux_metadata(*)")
     .eq("mock_interview_id", mockInterviewId)
     .order("created_at", { ascending: true });
 
-  if (messagesError) throw messagesError;
+  if (messagesError) {
+    logger.error("Error fetching mock interview messages", {
+      messagesError,
+    });
+    await logger.flush();
+    throw messagesError;
+  }
 
   // Fetch feedback
   const { data: feedback, error: feedbackError } = await supabase
@@ -30,7 +47,13 @@ async function fetchMockInterviewData(mockInterviewId: string) {
     .eq("mock_interview_id", mockInterviewId)
     .single();
 
-  if (feedbackError) throw feedbackError;
+  if (feedbackError) {
+    logger.error("Error fetching mock interview feedback", {
+      feedbackError,
+    });
+    await logger.flush();
+    throw feedbackError;
+  }
 
   // Fetch question feedback
   const { data: questionFeedback, error: questionFeedbackError } =
@@ -40,7 +63,13 @@ async function fetchMockInterviewData(mockInterviewId: string) {
       .eq("mock_interview_id", mockInterviewId)
       .order("created_at", { ascending: true });
 
-  if (questionFeedbackError) throw questionFeedbackError;
+  if (questionFeedbackError) {
+    logger.error("Error fetching mock interview question feedback", {
+      questionFeedbackError,
+    });
+    await logger.flush();
+    throw questionFeedbackError;
+  }
 
   // Generate signed URL for the recording only if recording_file_path exists
   let signedUrl: string | null = null;
